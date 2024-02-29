dd3d38eabe049cc7791970fbef4d216878c6341e
SanaaBou <boualilasanaa1@gmail.com>
2024-02-29 07:17:54 -0800

    simplify condition if

--- eureka_server/src/main/java/com/eai/eurekaserver/EurekaServerApplication.java
+++ eureka_server/src/main/java/com/eai/eurekaserver/EurekaServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurikaServerApplication {
+public class EurekaServerApplication {
     public static void main(String[] args) {
 
-        SpringApplication.run(EurikaServerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }}
