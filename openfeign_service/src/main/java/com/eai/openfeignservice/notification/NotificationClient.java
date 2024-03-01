be76907b1d4db7ff1ef2ef3c98ba95bd6a3c4e49
SanaaBou <boualilasanaa1@gmail.com>
2024-03-01 01:16:04 -0800

    simlify the end point

--- openfeign_service/src/main/java/com/eai/openfeignservice/notification/NotificationClient.java
+++ openfeign_service/src/main/java/com/eai/openfeignservice/notification/NotificationClient.java
@@ -3,13 +3,16 @@ package com.eai.openfeignservice.notification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("notification-service")
+@RequestMapping("/agd/notification-service")
+
 public interface NotificationClient {
 
-    @PostMapping("/agd/notification-service/send-email")
+    @PostMapping("send-email")
     String sendEmail(@RequestBody EmailSender request);
 
-    @PostMapping("/agd/notification-service/send-sms")
+    @PostMapping("send-sms")
     String sendSms(@RequestBody SmsSender request);
 }
--- openfeign_service/src/main/java/com/eai/openfeignservice/user/UserClient.java
+++ openfeign_service/src/main/java/com/eai/openfeignservice/user/UserClient.java
@@ -4,17 +4,19 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("client-service")
+@RequestMapping("/agd/client-service")
 public interface UserClient {
-    @PostMapping("/agd/client-service/email")
+    @PostMapping("email")
     Integer saveEmail(@RequestBody ClientRequest request);
 
-    @PostMapping("/agd/client-service/phone")
+    @PostMapping("phone")
     String addPhone(@RequestBody ClientRequest request);
 
 
-    @PostMapping("/agd/client-service/check-client")
+    @PostMapping("check-client")
     Boolean isClientExist(@RequestBody ClientRequest request);
 
 }
