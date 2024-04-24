438f6f64d8c2773be6fa819af17652494232f717
SanaaBou <boualilasanaa1@gmail.com>
2024-04-24 19:31:57 +0100

    delete unnecessary code

--- apigateway_service/src/main/java/com/eai/apigateway/filter/AuthenticationFilter.java
+++ apigateway_service/src/main/java/com/eai/apigateway/filter/AuthenticationFilter.java
@@ -12,7 +12,6 @@ import reactor.core.publisher.Mono;
 
 
 @Component
-@Slf4j
 public class AuthenticationFilter  extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
 
     @Autowired
@@ -29,14 +28,13 @@ public class AuthenticationFilter  extends AbstractGatewayFilterFactory<Authenti
         return ((exchange, chain) -> {
                 //header contains token or not
                 if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                    log.info("header authorization n'existe pas");
+
                     return Mono.error(new RuntimeException("missing authorization header"));
                 }
 
                 String requestTokenHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
 
-                log.info("!!!!!!!!!!");
-                log.info(requestTokenHeader);
+
 
                 if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
                     String jwtToken = requestTokenHeader.substring(7);
@@ -44,20 +42,18 @@ public class AuthenticationFilter  extends AbstractGatewayFilterFactory<Authenti
                     HttpHeaders headers = new HttpHeaders();
                     headers.setBearerAuth(jwtToken);
 
-                    log.info("here1");
-                    log.info(headers.toString());
+
                     HttpEntity<String> entity = new HttpEntity<>(headers);
                     restTemplate.postForEntity(
-                            "http://localhost:7777/agd/security-service/validateToken",
+                            "http://localhost:7777/agd/security-service/validateTokenService",
                             entity,
                             Void.class
                     );
-                    log.info("good");
+
                     return chain.filter(exchange);
                 }
 
 
-                log.info("requestTokenHeader est null");
             return chain.filter(exchange);
         });
     }
@@ -66,68 +62,6 @@ public class AuthenticationFilter  extends AbstractGatewayFilterFactory<Authenti
     public static class Config {
 
     }
-
-/*
-
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        if (routeValidator.isSecured.test(exchange.getRequest())) {
-            ServerHttpRequest request = exchange.getRequest();
-            String requestTokenHeader = request.getHeaders().getFirst("Authorization");
-
-
-            if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
-
-                String jwtToken = requestTokenHeader.substring(7);
-
-                HttpHeaders headers = new HttpHeaders();
-                headers.setBearerAuth(jwtToken);
-
-                log.info("here1");
-                log.info(jwtToken);
-                log.info(headers.toString());
-                //voici le resultat de ca: [Authorization:"Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJib3Vzc0BnbWFpb..]
-
-                /*
-                HttpEntity<String> entity = new HttpEntity<>(headers);
-                ResponseEntity<String> responseEntity = restTemplate.postForEntity(
-                        "http://localhost:9090/agd/security-service/get-email-from-token",
-                        entity,
-                        String.class
-                );
-
-                // Traitez la réponse ici
-                String emailFromToken = responseEntity.getBody();
-
-
-                SecurityRequestForAuthFilter securityRequest = new SecurityRequestForAuthFilter();
-                securityRequest.setJwtToken(jwtToken);
-
-
-
-                log.info("here2");
-                log.info(emailFromToken);
-                securityRequest.setEmailToken(emailFromToken);
-
-
-
- //here end of comment
-                // Appel à validateToken
-                //restTemplate.postForObject("http://localhost:9090/agd/security-service/validateToken", securityRequest, Void.class);
-                HttpEntity<String> entity = new HttpEntity<>(headers);
-                restTemplate.postForEntity(
-                        "http://localhost:9090/agd/security-service/validateToken",
-                        entity,
-                        Void.class
-                );
-                return chain.filter(exchange);
-            } else {
-                return Mono.error(new RuntimeException("Missing or invalid authorization header"));
-            }
-        } else {
-            return chain.filter(exchange);
-        }
-    }
-    */
 
 
 
