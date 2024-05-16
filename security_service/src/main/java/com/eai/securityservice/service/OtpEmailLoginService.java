a8ee63b35eabf67e94e03dcb3e608ae31f8a300a
IbtissameArrai <160483681+IbtissameArrai@users.noreply.github.com>
2024-05-16 10:27:01 +0100

    S4/75 76 77 78 79 security service partie ma demande (#48)
    * S4/75_76_77_78_79_security_service_partie_ma_demande
    
    * update OtpEmailService
    
    * update send-login-template
    
    * update OtpEmailLoginService
    
    ---------
    
    Co-authored-by: ayman elhamiani <59487962+ayman-elhamiani@users.noreply.github.com>

--- security_service/src/main/java/com/eai/securityservice/service/OtpEmailLoginService.java
+++ security_service/src/main/java/com/eai/securityservice/service/OtpEmailLoginService.java
@@ -111,8 +111,8 @@ public class OtpEmailLoginService {
         if (isPast30Minutes(otp.getDateGeneration()) < 30) {
             if (otp.getAttempts() < 3) {
                 Boolean isOtpValid = verifyOtp(otpEmailRequest.getUserInput(), otp.getCounter());
+                Integer idClient = otp.getIdClient();
 
-                Integer idClient = null;
                 otp.incrementAttempt();
                 otp.setIdClient(idClient);
                 otpRepository.save(otp);
@@ -128,7 +128,8 @@ public class OtpEmailLoginService {
                     String newGeneratedToken = jwtUtil.generateToken(userDetails, idClient);
 
                     otpEmailCompareResponse.setStatusOtp(StatusOTP.VALID.getLabel());
-                    otpEmailCompareResponse.setIdClient(idClient);
+                    otpEmailCompareResponse.setIdClient(identityClient.getIdClient());
+
                     otpEmailCompareResponse.setJwtToken(newGeneratedToken);
                     otpEmailCompareResponse.setStep(identityClient.getClientStep());
                     return otpEmailCompareResponse;
