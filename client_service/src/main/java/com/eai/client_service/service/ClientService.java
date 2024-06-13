ca4fed064747aaf6cf76769ebe1191541913aa2a
Your Name <you@example.com>
2024-06-13 14:48:34 +0100

    s7/100-recuperer-des-champs-du-backend

--- client_service/src/main/java/com/eai/client_service/service/ClientService.java
+++ client_service/src/main/java/com/eai/client_service/service/ClientService.java
@@ -151,6 +151,9 @@ public class ClientService {
                     .ville(client.getVille())
                     .agence(client.getAdresseAgence()+" "+client.getVilleAgence())
                     .pack(client.getPack().getNomPack())
+                    .offres(client.getPack().getOffres())
+                    .services(client.getPack().getServices())
+                    .dateCreation(client.getDateCreation())
                     .build();
             return clientResponseOcrDto;
         } else {
